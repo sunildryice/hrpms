cf26c4df697a424b2cd922c8033606801c3eb3f5
Sunil Adhikari <sunil@dryicesolutions.net>
2025-11-16 22:00:45 +0545

    logo and favicon and sidebar updated

--- resources/views/layouts/container.blade.php
+++ resources/views/layouts/container.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
-    <link rel="icon" type="image/png" href="{{ asset('img/slogo.png') }}">
+    <link rel="icon" type="image/png" href="{{ asset('img/favicon.png') }}">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <link rel="stylesheet" href="{!! mix('css/app.css') !!}">
     <link rel="stylesheet" href="{{ asset('stylesheets/style.min.css' . '?v=' . time()) }}">

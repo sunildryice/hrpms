b02dfa8d9956d1e2a8c2181543b7c17c659ed1f9
bpnpdl1 <bipinpaudel@dryicesolutions.net>
2025-11-25 17:01:40 +0545

    wip

--- modules/Employee/Views/SocialMedia/create.blade.php
+++ modules/Employee/Views/SocialMedia/create.blade.php
@@ -21,82 +21,18 @@
     <div class="row mb-2">
         <div class="col-lg-3">
             <div class="d-flex align-items-start h-100">
-                <label for="Fdname" class="form-label required-label">Training Topic</label>
+                <label for="linkedin" class="form-label">Linkedin</label>
             </div>
         </div>
         <div class="col-lg-9">
-            <input type="text" class="form-control @if ($errors->has('training_topic')) is-invalid @endif"
-                name="training_topic" placeholder="Training topic" value="{{ old('training_topic') }}">
-            @if ($errors->has('training_topic'))
+            <input type="text" class="form-control @if ($errors->has('linkedin')) is-invalid @endif"
+                name="linkedin" placeholder="Linkedin URL" value="{{ old('linkedin') }}">
+            @if ($errors->has('linkedin'))
                 <div class="fv-plugins-message-container invalid-feedback">
-                    <div data-field="training_topic">{!! $errors->first('training_topic') !!}</div>
+                    <div data-field="linkedin">{!! $errors->first('linkedin') !!}</div>
                 </div>
             @endif
         </div>
-    </div>
-
-    <div class="row mb-2">
-        <div class="col-lg-3">
-            <div class="d-flex align-items-start h-100">
-                <label for="validationdob" class="form-label required-label">Period From</label>
-            </div>
-        </div>
-        <div class="col-lg-9">
-            <input type="text" class="form-control @if ($errors->has('period_from')) is-invalid @endif"
-                name="period_from" placeholder="Period From" value="{{ old('period_from') }}" readonly>
-            @if ($errors->has('period_from'))
-                <div class="fv-plugins-message-container invalid-feedback">
-                    <div data-field="period_from">{!! $errors->first('period_from') !!}</div>
-                </div>
-            @endif
-        </div>
-    </div>
-    <div class="row mb-2">
-        <div class="col-lg-3">
-            <div class="d-flex align-items-start h-100">
-                <label for="validationdob" class="form-label required-label">Period To</label>
-            </div>
-        </div>
-        <div class="col-lg-9">
-            <input type="text" class="form-control @if ($errors->has('period_to')) is-invalid @endif"
-                readOnly:true, name="period_to" placeholder="Period To" value="{{ old('period_to') }}" readonly>
-            @if ($errors->has('period_to'))
-                <div class="fv-plugins-message-container invalid-feedback">
-                    <div data-field="period_to">{!! $errors->first('period_to') !!}</div>
-                </div>
-            @endif
-        </div>
-    </div>
-    <div class="row mb-2">
-        <div class="col-lg-3">
-            <div class="d-flex align-items-start h-100">
-                <label for="Fdname" class="form-label">Remarks</label>
-            </div>
-        </div>
-        <div class="col-lg-9">
-            <textarea name="remarks" class="form-control" placeholder="Remarks">{!! old('remarks') !!}</textarea>
-        </div>
-    </div>
-
-    <div class="row mb-2">
-        <div class="col-lg-3">
-            <div class="d-flex align-items-start h-100">
-                <label for="validationcitizenship" class="form-label">Attachment</label>
-            </div>
-        </div>
-        <div class="col-lg-9">
-            <input type="file" class="form-control @if ($errors->has('attachment')) is-invalid @endif"
-                name="attachment" />
-            <small>Supported file types jpeg/jpg/png/pdf and file size of upto 2MB.</small>
-            @if ($errors->has('attachment'))
-                <div class="fv-plugins-message-container invalid-feedback">
-                    <div data-field="attachment">{!! $errors->first('attachment') !!}</div>
-                </div>
-            @endif
-            {{--                <p>Only JPEG, PNG and PDF files are allowed.</p> --}}
-        </div>
-    </div>
-
     </div>
     <div class="card-footer border-0 justify-content-end d-flex gap-2">
         <button type="submit" class="btn btn-primary btn-sm">Save</button>
@@ -117,7 +53,7 @@
                             },
                         },
                     },
-                    training_topic: {
+                    linkedin: {
                         validators: {
                             notEmpty: {
                                 message: 'Training topic is required',

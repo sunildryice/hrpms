d3b3e257c23222c4ad85bdcf3b6c60e6c708a015
UD-Shrestha <uday@dryicesolutions.net>
2025-12-10 14:12:01 +0545

    TravelRequest : hide unnecessary fields in local-travel-itinerary

--- modules/TravelRequest/Views/LocalTravel/Itinerary/create.blade.php
+++ modules/TravelRequest/Views/LocalTravel/Itinerary/create.blade.php
@@ -2,8 +2,8 @@
     <h5 class="modal-title mb-0 fs-6" id="openModalLabel">Add New Detail</h5>
     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 </div>
-<form action="{!! route('local.travel.reimbursements.itineraries.store', $localTravel->id) !!}" method="post"
-      enctype="multipart/form-data" id="localTravelItineraryForm" autocomplete="off">
+<form action="{!! route('local.travel.reimbursements.itineraries.store', $localTravel->id) !!}" method="post" enctype="multipart/form-data" id="localTravelItineraryForm"
+    autocomplete="off">
     <div class="modal-body">
         <div class="row mb-2">
             <div class="col-lg-3">
@@ -12,10 +12,11 @@
                 </div>
             </div>
             <div class="col-lg-9">
-                <input type="text" class="form-control" readonly="readonly" name="travel_date" placeholder="Travel Date">
+                <input type="text" class="form-control" name="travel_date" onfocus="this.blur()"
+                    placeholder="yyyy-mm-dd">
             </div>
         </div>
-        <div class="row mb-2">
+        {{-- <div class="row mb-2">
             <div class="col-lg-3">
                 <div class="d-flex align-items-start h-100">
                     <label for="" class="form-label required-label">Purpose</label>
@@ -24,7 +25,7 @@
             <div class="col-lg-9">
                 <input type="text" class="form-control" name="purpose" value="" placeholder="Purpose">
             </div>
-        </div>
+        </div> --}}
         <div class="row mb-2">
             <div class="col-lg-3">
                 <div class="d-flex align-items-start h-100">
@@ -35,7 +36,8 @@
                 <input type="text" class="form-control" name="travel_mode" value="" placeholder="Travel Mode">
             </div>
         </div>
-        <div class="row mb-2">
+
+        {{-- <div class="row mb-2">
             <div class="col-lg-3">
                 <div class="d-flex align-items-start h-100">
                     <label for="" class="form-label required-label">Activity Code</label>
@@ -44,13 +46,14 @@
             <div class="col-lg-9">
                 <select class="form-control select2" data-width="100%" name="activity_code_id">
                     <option value="">Select Activity Code</option>
-                    @foreach($activityCodes as $activityCode)
+                    @foreach ($activityCodes as $activityCode)
                         <option value="{!! $activityCode->id !!}">{{ $activityCode->getActivityCodeWithDescription() }}</option>
                     @endforeach
                 </select>
             </div>
-        </div>
-        <div class="row mb-2">
+        </div> --}}
+
+        {{-- <div class="row mb-2">
             <div class="col-lg-3">
                 <div class="d-flex align-items-start h-100">
                     <label for="" class="form-label required-label">Account Code</label>
@@ -61,7 +64,8 @@
                     <option value="">Select Account Code</option>
                 </select>
             </div>
-        </div>
+        </div> --}}
+
         {{-- <div class="row mb-2">
             <div class="col-lg-3">
                 <div class="d-flex align-items-start h-100">
@@ -71,14 +75,14 @@
             <div class="col-lg-9">
                 <select class="form-control select2" data-width="100%" name="donor_code_id">
                     <option value="">Select Donor Code</option>
-                    @foreach($donorCodes as $donorCode)
+                    @foreach ($donorCodes as $donorCode)
                         <option value="{!! $donorCode->id !!}">{{ $donorCode->getDonorCodeWithDescription() }}</option>
                     @endforeach
                 </select>
             </div>
         </div> --}}
 
-        <div class="row mb-2">
+        {{-- <div class="row mb-2">
             <div class="col-lg-3">
                 <div class="d-flex align-items-start h-100">
                     <label for="" class="m-0">Departure Place</label>
@@ -87,8 +91,9 @@
             <div class="col-lg-9">
                 <input type="text" class="form-control" name="departure_place" value="" placeholder="Departure Place">
             </div>
-        </div>
-        <div class="row mb-2">
+        </div> --}}
+
+        {{-- <div class="row mb-2">
             <div class="col-lg-3">
                 <div class="d-flex align-items-start h-100">
                     <label for="" class="m-0">Arrival Place</label>
@@ -97,9 +102,9 @@
             <div class="col-lg-9">
                 <input type="text" class="form-control" name="arrival_place" value="" placeholder="Arrival Place">
             </div>
-        </div>
+        </div> --}}
 
-        <div class="row mb-2">
+        {{-- <div class="row mb-2">
             <div class="col-lg-3">
                 <div class="d-flex align-items-start h-100">
                     <label for="" class="m-0">Distance (in KM)</label>
@@ -108,9 +113,9 @@
             <div class="col-lg-9">
                 <input type="number" class="form-control" name="total_distance" value="" placeholder="Distance">
             </div>
-        </div>
+        </div> --}}
 
-        <div class="row mb-2">
+        {{-- <div class="row mb-2">
             <div class="col-lg-3">
                 <div class="d-flex align-items-start h-100">
                     <label for="" class="form-label required-label">Fare</label>
@@ -119,12 +124,12 @@
             <div class="col-lg-6">
                 <input type="number" class="form-control" name="total_fare" value="" placeholder="Fare">
             </div>
-        </div>
+        </div> --}}
 
         <div class="row mb-2">
             <div class="col-lg-3">
                 <div class="d-flex align-items-start h-100">
-                    <label for="" class="m-0">Remarks</label>
+                    <label for="" class="m-0">Reason of the travel details</label>
                 </div>
             </div>
             <div class="col-lg-9">
--- modules/TravelRequest/Views/LocalTravel/Itinerary/edit.blade.php
+++ modules/TravelRequest/Views/LocalTravel/Itinerary/edit.blade.php
@@ -2,8 +2,11 @@
     <h5 class="modal-title mb-0 fs-6" id="openModalLabel">Edit Detail</h5>
     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 </div>
-<form action="{!! route('local.travel.reimbursements.itineraries.update', [$localTravelItinerary->local_travel_reimbursement_id, $localTravelItinerary->id]) !!}" method="post"
-      enctype="multipart/form-data" id="localTravelItineraryForm" autocomplete="off">
+<form action="{!! route('local.travel.reimbursements.itineraries.update', [
+    $localTravelItinerary->local_travel_reimbursement_id,
+    $localTravelItinerary->id,
+]) !!}" method="post" enctype="multipart/form-data" id="localTravelItineraryForm"
+    autocomplete="off">
     <div class="modal-body">
         <div class="row mb-2">
             <div class="col-lg-3">
@@ -12,10 +15,12 @@
                 </div>
             </div>
             <div class="col-lg-9">
-                <input type="text" class="form-control" readonly="readonly" value="{{ $localTravelItinerary->travel_date->format('Y-m-d') }}" name="travel_date" placeholder="Travel Date">
+                <input type="text" class="form-control"
+                    value="{{ $localTravelItinerary->travel_date->format('Y-m-d') }}" name="travel_date"
+                    onfocus="this.blur()" placeholder="yyyy-mm-dd">
             </div>
         </div>
-        <div class="row mb-2">
+        {{-- <div class="row mb-2">
             <div class="col-lg-3">
                 <div class="d-flex align-items-start h-100">
                     <label for="" class="form-label required-label">Purpose</label>
@@ -24,7 +29,7 @@
             <div class="col-lg-9">
                 <input type="text" class="form-control" name="purpose" value="{{ $localTravelItinerary->purpose }}" placeholder="Purpose">
             </div>
-        </div>
+        </div> --}}
         <div class="row mb-2">
             <div class="col-lg-3">
                 <div class="d-flex align-items-start h-100">
@@ -32,10 +37,11 @@
                 </div>
             </div>
             <div class="col-lg-9">
-                <input type="text" class="form-control" name="travel_mode" value="{{ $localTravelItinerary->travel_mode }}" placeholder="Travel Mode">
+                <input type="text" class="form-control" name="travel_mode"
+                    value="{{ $localTravelItinerary->travel_mode }}" placeholder="Travel Mode">
             </div>
         </div>
-        <div class="row mb-2">
+        {{-- <div class="row mb-2">
             <div class="col-lg-3">
                 <div class="d-flex align-items-start h-100">
                     <label for="" class="form-label required-label">Activity Code</label>
@@ -44,13 +50,14 @@
             <div class="col-lg-9">
                 <select class="form-control select2" data-width="100%" name="activity_code_id">
                     <option value="">Select Activity Code</option>
-                    @foreach($activityCodes as $activityCode)
-                        <option value="{!! $activityCode->id !!}" @if($activityCode->id == $localTravelItinerary->activity_code_id) selected @endif>{{ $activityCode->getActivityCodeWithDescription() }}</option>
+                    @foreach ($activityCodes as $activityCode)
+                        <option value="{!! $activityCode->id !!}" @if ($activityCode->id == $localTravelItinerary->activity_code_id) selected @endif>{{ $activityCode->getActivityCodeWithDescription() }}</option>
                     @endforeach
                 </select>
             </div>
-        </div>
-        <div class="row mb-2">
+        </div> --}}
+
+        {{-- <div class="row mb-2">
             <div class="col-lg-3">
                 <div class="d-flex align-items-start h-100">
                     <label for="" class="form-label required-label">Account Code</label>
@@ -59,12 +66,13 @@
             <div class="col-lg-9">
                 <select class="form-control select2" data-width="100%" name="account_code_id">
                     <option value="">Select Account Code</option>
-                    @foreach($accountCodes as $accountCode)
-                        <option value="{!! $accountCode->id !!}" @if($accountCode->id == $localTravelItinerary->account_code_id) selected @endif>{{ $accountCode->getAccountCodeWithDescription() }}</option>
+                    @foreach ($accountCodes as $accountCode)
+                        <option value="{!! $accountCode->id !!}" @if ($accountCode->id == $localTravelItinerary->account_code_id) selected @endif>{{ $accountCode->getAccountCodeWithDescription() }}</option>
                     @endforeach
                 </select>
             </div>
-        </div>
+        </div> --}}
+
         {{-- <div class="row mb-2">
             <div class="col-lg-3">
                 <div class="d-flex align-items-start h-100">
@@ -74,14 +82,14 @@
             <div class="col-lg-9">
                 <select class="form-control select2" data-width="100%" name="donor_code_id">
                     <option value="">Select Donor Code</option>
-                    @foreach($donorCodes as $donorCode)
-                        <option value="{!! $donorCode->id !!}" @if($donorCode->id == $localTravelItinerary->donor_code_id) selected @endif>{{ $donorCode->getDonorCodeWithDescription() }}</option>
+                    @foreach ($donorCodes as $donorCode)
+                        <option value="{!! $donorCode->id !!}" @if ($donorCode->id == $localTravelItinerary->donor_code_id) selected @endif>{{ $donorCode->getDonorCodeWithDescription() }}</option>
                     @endforeach
                 </select>
             </div>
         </div> --}}
 
-        <div class="row mb-2">
+        {{-- <div class="row mb-2">
             <div class="col-lg-3">
                 <div class="d-flex align-items-start h-100">
                     <label for="" class="m-0">Departure Place</label>
@@ -90,8 +98,9 @@
             <div class="col-lg-9">
                 <input type="text" class="form-control" name="departure_place" value="{{ $localTravelItinerary->departure_place }}" placeholder="Departure Place">
             </div>
-        </div>
-        <div class="row mb-2">
+        </div> --}}
+
+        {{-- <div class="row mb-2">
             <div class="col-lg-3">
                 <div class="d-flex align-items-start h-100">
                     <label for="" class="m-0">Arrival Place</label>
@@ -100,9 +109,9 @@
             <div class="col-lg-9">
                 <input type="text" class="form-control" name="arrival_place" value="{{ $localTravelItinerary->arrival_place }}" placeholder="Arrival Place">
             </div>
-        </div>
+        </div> --}}
 
-        <div class="row mb-2">
+        {{-- <div class="row mb-2">
             <div class="col-lg-3">
                 <div class="d-flex align-items-start h-100">
                     <label for="" class="m-0">Distance (in KM)</label>
@@ -111,9 +120,9 @@
             <div class="col-lg-9">
                 <input type="number" class="form-control" name="total_distance" value="{{ $localTravelItinerary->total_distance }}" placeholder="Distance">
             </div>
-        </div>
+        </div> --}}
 
-        <div class="row mb-2">
+        {{-- <div class="row mb-2">
             <div class="col-lg-3">
                 <div class="d-flex align-items-start h-100">
                     <label for="" class="form-label required-label">Fare</label>
@@ -122,12 +131,12 @@
             <div class="col-lg-6">
                 <input type="number" class="form-control" name="total_fare" value="{{ $localTravelItinerary->total_fare }}" placeholder="Fare">
             </div>
-        </div>
+        </div> --}}
 
         <div class="row mb-2">
             <div class="col-lg-3">
                 <div class="d-flex align-items-start h-100">
-                    <label for="" class="m-0">Remarks</label>
+                    <label for="" class="m-0">Reason of the travel details</label>
                 </div>
             </div>
             <div class="col-lg-9">

bda9557a61c9829a1052ce991bbb0223976d3056
UD-Shrestha <uday@dryicesolutions.net>
2025-12-19 15:47:00 +0545

    Employee : update FormRequest for Consultant

--- modules/Employee/Requests/Consultant/StoreRequest.php
+++ modules/Employee/Requests/Consultant/StoreRequest.php
@@ -42,6 +42,13 @@ class StoreRequest extends FormRequest
             'date_of_birth'=>'nullable|date',
             'religion_id'=>'nullable',
             'caste_id'=>'nullable',
+            
+            'nid_number' => 'nullable|string|max:50',
+            'passport_number' => 'nullable|string|max:20',
+            'passport_attachment' => 'nullable|mimes:jpg,jpeg,png,pdf|max:2048',
+            'vehicle_license_number' => 'nullable|string|max:50',
+            'vehicle_license_category' => 'nullable|array',
+            'vehicle_license_category.*' => 'string|in:A,B,C,D,E,F,G,H,K',
         ];
     }
 

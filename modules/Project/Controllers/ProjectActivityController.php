62c2a324668b7a81e521bed9b7c7f94215932bf0
bpnpdl1 <bipinpaudel@dryicesolutions.net>
2026-01-20 10:59:45 +0545

    feat: added project activites routes

--- modules/Project/Controllers/ProjectActivityController.php
+++ modules/Project/Controllers/ProjectActivityController.php
@@ -4,11 +4,17 @@ namespace Modules\Project\Controllers;
 
 use App\Http\Controllers\Controller;
 use Modules\Project\Requests\ProjectActivity\StoreRequest;
+use Modules\Project\Requests\ProjectActivity\UpdateRequest;
 use Modules\Project\Models\Enums\ActivityLevel;
 use Modules\Project\Models\Project;
+use Modules\Project\Repositories\ProjectActivityRepository;
 
 class ProjectActivityController extends Controller
 {
+    public function __construct(
+        protected ProjectActivityRepository $projectActivity
+    ) {}
+
     public function create(Project $project)
     {
         $activityLevels = ActivityLevel::cases();
@@ -21,6 +27,37 @@ class ProjectActivityController extends Controller
 
     public function store(StoreRequest $request)
     {
-        dd($request->all());
+        $input = $request->validated();
+        $this->projectActivity->create($input);
+
+        return redirect()->route('project.show', $input['project_id'])
+            ->with('success_message', 'Project Activity created successfully.');
+    }
+
+    public function edit($id)
+    {
+        $projectActivity = $this->projectActivity->find($id);
+        $activityLevels = ActivityLevel::cases();
+        $stages = $projectActivity->project->stages;
+        // $parentActivities = $projectActivity->project->activities;
+
+        return view('Project::ProjectActivity.edit', compact('projectActivity', 'activityLevels', 'stages'));
+    }
+
+    public function update(UpdateRequest $request, $id)
+    {
+        $input = $request->validated();
+        $this->projectActivity->update($id, $input);
+
+        return redirect()->route('project.show', $input['project_id'])
+            ->with('success_message', 'Project Activity updated successfully.');
+    }
+
+    public function destroy($id)
+    {
+        $this->projectActivity->destroy($id);
+
+        return redirect()->route('project.show', $id)
+            ->with('success_message', 'Project Activity deleted successfully.');
     }
 }

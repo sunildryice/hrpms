8f1f14be5abf702400b068535351b92fde88c22f
UD-Shrestha <uday@dryicesolutions.net>
2026-01-21 10:31:20 +0545

    Project: update store and update method and update repo in project

--- modules/Project/Controllers/ProjectController.php
+++ modules/Project/Controllers/ProjectController.php
@@ -17,7 +17,8 @@ class ProjectController
         protected ProjectRepository $projectRepository,
         protected UserRepository $userRepository,
         protected ActivityStageRepository $activityStageRepository,
-    ) {}
+    ) {
+    }
 
     public function index(Request $request)
     {
@@ -38,7 +39,7 @@ class ProjectController
                     $btn .= route('project.show', $row->id) . '" rel="tooltip" title="View Project">';
                     $btn .= '<i class="bi bi-eye"></i></a>';
 
-                    $btn  .= ' <a class="btn btn-outline-primary btn-sm" href="';
+                    $btn .= ' <a class="btn btn-outline-primary btn-sm" href="';
                     $btn .= route('project.edit', $row->id) . '" rel="tooltip" title="Edit Project">';
                     $btn .= '<i class="bi bi-pencil-square"></i></a>';
 
@@ -70,22 +71,11 @@ class ProjectController
     public function store(StoreRequest $request)
     {
         $inputs = $request->validated();
-
-        try {
-            $stages = $inputs['stages'];
-            $members = $inputs['members'];
-
-            $project = $this->projectRepository->create($inputs);
-
-            $project->stages()->sync($stages);
-
-            $project->members()->sync($members);
-
-
+        $project = $this->projectRepository->create($inputs);
+        if ($project) {
             return redirect()->route('project.edit', $project->id)->withSuccessMessage('Project created successfully.');
-        } catch (\Exception $e) {
-            return redirect()->back()->withInput()->withErrorMessage('Failed to create Project. ' . $e->getMessage());
         }
+        return redirect()->back()->withInput()->withErrorMessage('Failed to create Project.');
     }
 
     public function show($id)
@@ -99,7 +89,7 @@ class ProjectController
 
     public function edit($id)
     {
-        $project = $this->projectRepository->with('members', 'stages')->find($id);
+        $project = $this->projectRepository->with(['members', 'stages'])->find($id);
         $users = $this->userRepository->pluck('full_name', 'id');
         $stages = $this->activityStageRepository->all();
         return view('Project::Project.edit', compact('project', 'users', 'stages'));
@@ -108,18 +98,11 @@ class ProjectController
     public function update($id, UpdateRequest $request)
     {
         $inputs = $request->validated();
-
-        $this->projectRepository->update($id, $inputs);
-
-        $project = $this->projectRepository->find($id);
-        $stages = $inputs['stages'];
-        $members = $inputs['members'];
-        $project->stages()->sync($stages);
-        $project->members()->sync($members);
-
-
-
-        return redirect()->route('project.index')->withSuccessMessage('Project updated successfully.');
+        $project = $this->projectRepository->update($id, $inputs);
+        if ($project) {
+            return redirect()->route('project.index')->withSuccessMessage('Project updated successfully.');
+        }
+        return redirect()->back()->withInput()->withErrorMessage('Failed to update Project.');
     }
 
     public function destroy($id)
--- modules/Project/Repositories/ProjectRepository.php
+++ modules/Project/Repositories/ProjectRepository.php
@@ -2,6 +2,7 @@
 
 namespace Modules\Project\Repositories;
 
+use DB;
 use App\Repositories\Repository;
 use Modules\Project\Models\Project;
 
@@ -10,5 +11,51 @@ class ProjectRepository extends Repository
     public function __construct(Project $model)
     {
         $this->model = $model;
+    }
+
+    public function create($inputs)
+    {
+        DB::beginTransaction();
+        try {
+            $project = $this->model->create($inputs);
+            if (!empty($inputs['stages'])) {
+                $project->stages()->sync($inputs['stages']);
+            }
+            if (!empty($inputs['members'])) {
+                $project->members()->sync($inputs['members']);
+            }
+            DB::commit();
+            return $project;
+        } catch (\Illuminate\Database\QueryException $e) {
+            DB::rollback();
+            return false;
+        }
+    }
+
+    public function update($id, $inputs)
+    {
+        DB::beginTransaction();
+        try {
+            $project = $this->model->find($id);
+            $project->fill($inputs)->save();
+
+            if (!empty($inputs['stages'])) {
+                $project->stages()->sync($inputs['stages']);
+            } else {
+                $project->stages()->sync([]);
+            }
+
+            if (!empty($inputs['members'])) {
+                $project->members()->sync($inputs['members']);
+            } else {
+                $project->members()->sync([]);
+            }
+            DB::commit();
+
+            return $project;
+        } catch (\Illuminate\Database\QueryException $e) {
+            DB::rollback();
+            return false;
+        }
     }
 }

e6e07af6fd67a8d6150e91f7f6784310bb5297f3
UD-Shrestha <uday@dryicesolutions.net>
2026-01-22 15:40:59 +0545

    Project: add ActivityTimeSheet Model

---
+++ modules/Project/Models/ActivityTimeSheet.php
@@ -0,0 +1,43 @@
+<?php 
+
+namespace Modules\Project\Models;
+
+use Modules\Privilege\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class ActivityTimeSheet extends Model
+{
+    use HasFactory;
+
+    protected $table = 'project_activity_timesheet';
+
+    protected $fillable = [
+        'project_id',
+        'activity_id',
+        'user_id',
+        'timesheet_date',
+        'hours_spent',
+        'description',
+        'created_by',
+        'updated_by',
+    ];
+    protected $dates = [
+        'timesheet_date',
+    ];
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class, 'project_id');
+    }
+
+    public function activity()
+    {
+        return $this->belongsTo(ProjectActivity::class, 'activity_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}

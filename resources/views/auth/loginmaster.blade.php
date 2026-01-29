374a46458fc19f801fe443c3558cb4612bc3f317
Sunil Adhikari <sunil@dryicesolutions.net>
2026-01-29 14:09:47 +0545

    login check

--- resources/views/auth/loginmaster.blade.php
+++ resources/views/auth/loginmaster.blade.php
@@ -11,14 +11,8 @@
     <title>@yield('title')</title>
     <!--Core CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
-    @if(app()->environment('staging'))x
-        <link rel="stylesheet" href="http://hrpms.example/css/app.css">
-        <link rel="stylesheet" href="http://202.79.34.201:8080/css/app.css">
-        <link rel="stylesheet" href="http://202.79.34.201:8080/stylesheets/style.css">
-    @else
-        <link rel="stylesheet" href="{!! asset('css/app.css') !!}">
-        <link rel="stylesheet" href="{!! asset('stylesheets/style.css') !!}">
-    @endif
+    <link rel="stylesheet" href="{!! asset('css/app.css') !!}">
+    <link rel="stylesheet" href="{!! asset('stylesheets/style.css') !!}">
 </head>
 
 <body class="login-body bg-light">
@@ -40,11 +34,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
         integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous">
 </script>
-@if(app()->environment('staging'))
-    <script src="http://202.79.34.201:8080/js/app.js"></script>
-@else
-    <script src="{!! asset('js/app.js') !!}"></script>
-@endif
+<script src="{!! asset('js/app.js') !!}"></script>
 <script type="text/javascript">
     var rootUrl = '{!! url('') !!}';
     $(document).ready(function () {

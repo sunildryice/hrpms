b0e595da3839a8a5c93042071e6de7c4f8bdee7b
Sunil Adhikari <sunil@dryicesolutions.net>
2026-01-29 13:58:57 +0545

    login check

--- resources/views/auth/loginmaster.blade.php
+++ resources/views/auth/loginmaster.blade.php
@@ -7,43 +7,53 @@
     <meta name="description" content="">
     <meta name="author" content="DryIce Solutions Pvt Ltd">
     <link rel="shortcut icon" href="{{ asset('img/favicon.png') }}">
-    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <meta name="csrf-token" content="{{ csrf_token() }}"/>
     <title>@yield('title')</title>
     <!--Core CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="{!! asset('css/app.css') !!}">
-    <link rel="stylesheet" href="{!! asset('stylesheets/style.css') !!}">
+    @if(app()->environment('staging'))
+        <link rel="stylesheet" href="http://202.79.34.201:8080/css/app.css">
+        <link rel="stylesheet" href="http://202.79.34.201:8080/stylesheets/style.css">
+    @else
+        <link rel="stylesheet" href="{!! asset('css/app.css') !!}">
+        <link rel="stylesheet" href="{!! asset('stylesheets/style.css') !!}">
+    @endif
 </head>
 
 <body class="login-body bg-light">
 
-    <div class="container">
-        <div class="mx-auto d-flex align-items-center justify-content-center " style="max-width: 30rem; height: 100vh;">
+<div class="container">
+    <div class="mx-auto d-flex align-items-center justify-content-center " style="max-width: 30rem; height: 100vh;">
 
 
-            @yield('page-content')
+        @yield('page-content')
 
 
-        </div>
     </div>
+</div>
 
 
-    <!--Core js-->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"
+<!--Core js-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"
         integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ=="
         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
         integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
+</script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
         integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous">
-    </script>
+</script>
+@if(app()->environment('staging'))
+    <script src="http://202.79.34.201:8080/js/app.js"></script>
+@else
     <script src="{!! asset('js/app.js') !!}"></script>
-    <script type="text/javascript">
-        var rootUrl = '{!! url('') !!}';
-        $(document).ready(function() {});
-    </script>
-    @stack('scripts')
+@endif
+<script type="text/javascript">
+    var rootUrl = '{!! url('') !!}';
+    $(document).ready(function () {
+    });
+</script>
+@stack('scripts')
 </body>
 
 </html>

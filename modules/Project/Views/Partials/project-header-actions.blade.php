70b75c0c67231bf28e2c42c91ed50cf580fbcdb3
UD-Shrestha <uday@dryicesolutions.net>
2026-01-30 17:50:32 +0545

    Project: update project-header-actions

--- modules/Project/Views/Partials/project-header-actions.blade.php
+++ modules/Project/Views/Partials/project-header-actions.blade.php
@@ -1,19 +1,22 @@
- <div>
-     <a href="{{ route('project.dashboard', $project->id) }}"
-         class="btn 
-        {{ request()->routeIs('project.dashboard') ? 'btn-outline-primary' : 'btn-primary' }}
-        btn-sm">
-         Dashboard
-     </a>
-     <a href="{{ route('project.show', $project->id) }}"
-         class="btn 
-        {{ request()->routeIs('project.show') ? 'btn-outline-primary' : 'btn-primary' }}
-        btn-sm">
-         Activities
-     </a>
-     <a href="{{ route('project.gantt.index', ['id' => $project->id]) }}"
-         class="btn 
-        {{ request()->routeIs('project.gantt.index') ? 'btn-outline-primary' : 'btn-primary' }} btn-sm">
-         Gantt
-         Chart</a>
- </div>
+<div>
+    <a href="{{ route('project.dashboard', $project->id) }}"
+        class="btn 
+              {{ request()->routeIs('project.dashboard') ? 'btn-primary' : 'btn-outline-primary' }}
+              btn-sm">
+        Dashboard
+    </a>
+
+    <a href="{{ route('project.show', $project->id) }}"
+        class="btn 
+              {{ request()->routeIs('project.show') ? 'btn-primary' : 'btn-outline-primary' }}
+              btn-sm">
+        Activities
+    </a>
+
+    <a href="{{ route('project.gantt.index', ['id' => $project->id]) }}"
+        class="btn 
+              {{ request()->routeIs('project.gantt.index') ? 'btn-primary' : 'btn-outline-primary' }} 
+              btn-sm">
+        Gantt Chart
+    </a>
+</div>

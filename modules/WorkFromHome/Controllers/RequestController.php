03694d5f84911ea04a801c7adbd21c3fc6852a49
bpnpdl1 <bipinpaudel@dryicesolutions.net>
2026-01-30 10:27:07 +0545

    wip

--- modules/WorkFromHome/Controllers/RequestController.php
+++ modules/WorkFromHome/Controllers/RequestController.php
@@ -11,6 +11,7 @@ use Modules\WorkFromHome\Requests\UpdateRequest;
 use Modules\Master\Repositories\FiscalYearRepository;
 use Modules\Master\Repositories\ProjectCodeRepository;
 use Modules\Privilege\Repositories\UserRepository;
+use Modules\Project\Repositories\ProjectRepository;
 use Modules\WorkFromHome\Notifications\WorkFromHomeRequestSubmitted;
 use Modules\WorkFromHome\Repositories\WorkFromHomeLogRepository;
 use Modules\WorkFromHome\Repositories\WorkFromHomeRepository;
@@ -20,7 +21,7 @@ class RequestController extends Controller
 {
 
     public function __construct(
-        protected ProjectCodeRepository $projects,
+        protected ProjectRepository $projects,
         protected UserRepository $users,
         protected WorkFromHomeRepository $workFromHomes,
         protected WorkFromHomeLogRepository $workFromHomeLogs,
@@ -88,7 +89,12 @@ class RequestController extends Controller
 
         $authUser = auth()->user();
 
-        $projects = $this->projects->pluck('short_name', 'id')->toArray();
+        $projects = $this->projects
+            ->with('activities')
+            ->whereHas('members', function ($q) use ($authUser) {
+                $q->where('user_id', $authUser->id);
+            })
+            ->get();
         $supervisors = $this->users->getSupervisors($authUser)->pluck('full_name', 'id');
 
         return view('WorkFromHome::create', [

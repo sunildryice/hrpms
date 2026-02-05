0376da9d0aba35661c9fbbb3c316f4a71df2a94a
UD-Shrestha <uday@dryicesolutions.net>
2026-02-05 15:05:00 +0545

    Project: add log in in the show page of the monthly timesheet

--- modules/Project/Views/MonthlyTimeSheet/show.blade.php
+++ modules/Project/Views/MonthlyTimeSheet/show.blade.php
@@ -168,7 +168,7 @@
                     </table>
                 </div>
             </div>
-           @if ($authUser->can('submit', $timeSheet))
+            @if ($authUser->can('submit', $timeSheet))
                 <div class="card-footer border-top">
                     <form action="{{ route('monthly-timesheet.update', $timeSheet->id) }}" method="POST">
                         @csrf
@@ -218,11 +218,47 @@
                     </form>
                 </div>
             @else
-                <div class="card-footer bg-light text-center py-3">
-                    <small class="text-muted">
-                        Approval form will be available after the period ends on
-                        <strong>{{ $timeSheet->end_date->format('d M Y') }}</strong>
-                    </small>
+                @if ($timeSheet->status_id == config('constant.CREATED_STATUS'))
+                    <div class="card-footer bg-light text-center py-3">
+                        <small class="text-muted">
+                            Approval form will be available after the period ends on
+                            <strong>{{ $timeSheet->end_date->format('d M Y') }}</strong>
+                        </small>
+                    </div>
+                @endif
+            @endif
+            @if (in_array($timeSheet->status_id, [config('constant.APPROVED_STATUS'), config('constant.SUBMITTED_STATUS')]))
+                <div class="card">
+                    <div class="card-header fw-bold">Approval & Comments History</div>
+                    <div class="card-body">
+                        <div class="c-b">
+                            @foreach ($timeSheet->logs as $log)
+                                <div class="d-flex py-2 flex-row gap-2 mb-2 border-bottom">
+                                    <div class="rounded-circle user-icon">
+                                        <i class="bi-person-circle fs-5"></i>
+                                    </div>
+                                    <div class="w-100">
+                                        <div
+                                            class="d-flex flex-column flex-md-row justify-content-between align-items-start align-items-md-center">
+                                            <div
+                                                class="d-flex flex-column flex-md-row align-items-start align-items-md-center gap-md-2 mb-2 mb-md-0">
+                                                <label class="form-label mb-0">{{ $log->getCreatedBy() }}</label>
+                                                <span class="badge bg-primary c-badge">
+                                                    {!! $log->createdBy->employee->latestTenure->getDesignationName() !!}
+                                                </span>
+                                            </div>
+                                            <small title="{{ $log->created_at }}">
+                                                {{ $log->created_at->format('M d, Y h:i A') }}
+                                            </small>
+                                        </div>
+                                        <p class="text-justify comment-text mb-0 mt-1">
+                                            {{ $log->log_remarks }}
+                                        </p>
+                                    </div>
+                                </div>
+                            @endforeach
+                        </div>
+                    </div>
                 </div>
             @endif
         </div>

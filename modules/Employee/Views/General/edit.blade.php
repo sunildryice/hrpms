5b78d3739e3d71d4e85a6d15e84c0e5249259a7b
UD-Shrestha <uday@dryicesolutions.net>
2026-02-10 10:59:03 +0545

    wip

--- modules/Employee/Views/General/edit.blade.php
+++ modules/Employee/Views/General/edit.blade.php
@@ -97,7 +97,7 @@
         <div class="row mb-2">
             <div class="col-lg-3">
                 <div class="d-flex align-items-start h-100">
-                    <label for="validationdob" class="m-0">{{ __('label.from-date') }}
+                    <label for="validationdob" class="m-0">Joined Date
                     </label>
                 </div>
             </div>

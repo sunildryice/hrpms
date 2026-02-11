df91f3f57145e9f08ff19a66c1b1799e4410a759
Sunil Adhikari <sunil@dryicesolutions.net>
2026-02-12 00:55:41 +0545

    log file permission on logging

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -21,7 +21,6 @@ class Kernel extends ConsoleKernel
         $schedule->command('dryice:update:attendance:working:hour')->dailyAt('01:30');
         $schedule->command('dryice:make:workplan')->weekly();
         $schedule->command('dryice:import:attendance ')->hourlyAt(16);
-        //         $schedule->command('dryice:send-employee-exit-payable-settlement-notification')->dailyAt('00:05');
     }
 
     /**

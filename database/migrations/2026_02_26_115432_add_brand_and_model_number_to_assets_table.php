3e20643f98348365659a8904ccc39d4931e0aff4
UD-Shrestha <uday@dryicesolutions.net>
2026-02-26 11:57:47 +0545

    Add brand and model_number in the assets table

---
+++ database/migrations/2026_02_26_115432_add_brand_and_model_number_to_assets_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('assets', function (Blueprint $table) {
+            $table->string('brand')->nullable()->after('serial_number');
+            $table->string('model_number')->nullable()->after('brand');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('assets', function (Blueprint $table) {
+            $table->dropColumn(['model_number', 'brand']);
+        });
+    }
+};

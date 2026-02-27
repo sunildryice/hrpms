c2254c910d7b4d7b4c0ff214cc6a11275c6b51c6
UD-Shrestha <uday@dryicesolutions.net>
2026-02-27 15:04:25 +0545

    TravelClaim : update print for travel claim

--- modules/TravelRequest/Controllers/ClaimApprovedController.php
+++ modules/TravelRequest/Controllers/ClaimApprovedController.php
@@ -20,10 +20,9 @@ class ClaimApprovedController extends Controller
      * @param UserRepository $users
      */
     public function __construct(
-        TravelClaimRepository     $travelClaims,
+        TravelClaimRepository $travelClaims,
         UserRepository $users
-    )
-    {
+    ) {
         $this->travelClaims = $travelClaims;
         $this->users = $users;
     }
@@ -41,12 +40,12 @@ class ClaimApprovedController extends Controller
             $data = $this->travelClaims->with([
                 'travelRequest' => function ($q) {
                     $q->with(['fiscalYear'])
-                        ->select(['id', 'departure_date', 'return_date','final_destination', 'prefix', 'travel_number', 'modification_number','fiscal_year_id']);
+                        ->select(['id', 'departure_date', 'return_date', 'final_destination', 'prefix', 'travel_number', 'modification_number', 'fiscal_year_id']);
                 },
-                 'requester',
-                  'status'
-                  ])->select(['*'])
-                ->whereIn('status_id',[config('constant.APPROVED_STATUS'),config('constant.PAID_STATUS')])
+                'requester',
+                'status'
+            ])->select(['*'])
+                ->whereIn('status_id', [config('constant.APPROVED_STATUS'), config('constant.PAID_STATUS')])
                 ->orderBy('created_at', 'desc')
                 ->get();
 
@@ -64,8 +63,8 @@ class ClaimApprovedController extends Controller
                     return $row->travelRequest->getTravelRequestNumber();
                 })->addColumn('status', function ($row) {
                     return '<span class="' . $row->getStatusClass() . '">' . $row->getStatus() . '</span>';
-                })->addColumn('status', function ($row){
-                    return '<span class="'.$row->getStatusClass() .'">'.$row->getStatus().'</span>';
+                })->addColumn('status', function ($row) {
+                    return '<span class="' . $row->getStatusClass() . '">' . $row->getStatus() . '</span>';
                 })->addColumn('action', function ($row) use ($authUser) {
                     $btn = '<a class="btn btn-outline-primary btn-sm" href="';
                     $btn .= route('travel.claims.view', $row->id) . '" rel="tooltip" title="View Travel Claim">';
@@ -73,7 +72,7 @@ class ClaimApprovedController extends Controller
 
                     $btn .= '&emsp;<a class="btn btn-outline-primary btn-sm" target="_blank" href="';
                     $btn .= route('approved.travel.claims.print', $row->id) . '" rel="tooltip" title="Print Travel Claim"><i class="bi bi-printer"></i></a>';
-                    if ($authUser->can('pay',$row)){
+                    if ($authUser->can('pay', $row)) {
                         $btn .= '&emsp;<button class="btn btn-outline-success btn-sm open-payment-modal-form" href="';
                         $btn .= route('approved.travel.claims.pay.create', $row->id) . '" rel="tooltip" title="Pay"><i class="bi bi-cash-stack"></i></button>';
                     }
@@ -93,49 +92,26 @@ class ClaimApprovedController extends Controller
      */
     public function print($id)
     {
-        $authUser = auth()->user();
         $travelClaim = $this->travelClaims->find($id);
         $this->authorize('printApproved', $travelClaim);
 
-        $itineraries = $travelClaim->itineraries()->with(['travelRequestItinerary.activityCode', 'travelRequestItinerary.donorCode', 'office'])
-        ->select(['travel_claim_itineraries.*', 'i2.activity_code_id'])
-        ->join('travel_request_itineraries as i2','i2.id','=', 'travel_claim_itineraries.travel_itinerary_id')
-        ->get();
+        $travelClaim->load([
+            'dsaClaims.activityCode',
+            'localTravels.activityCode',
+            'expenses.activityCode',
+            'travelRequest.office',
+            'requester.employee',
+            'reviewer.employee',
+            'recommender.employee',
+            'approver.employee',
+            'logs',
+            'approvedLog',
+            'reviewedLog',
+            'recommendedLog',
+            'submittedLog',
+        ]);
 
-        $itineraries = $itineraries->groupBy(['activity_code_id', 'donor_code_id', 'office_id'])->flatten(2)
-        ->map(function($itinerary){
-            $dsa = collect();
-            foreach($itinerary as $index => $value){
-                if($index == 0){
-                    $dsa = $value;
-                    continue;
-                }
-                $dsa->total_amount += $value->total_amount;
-            }
-            $dsa->subledger = 'DSA';
-            return $dsa;
-        });
-        $expenses = $travelClaim->expenses()->with(['activityCode', 'donorCode', 'office'])->get();
-        
-        $expenses = $expenses->groupBy(['activity_code_id', 'donor_code_id', 'office_id'])->flatten(2)
-            ->map(function($expense){
-                $travel = collect();
-                foreach($expense as $index => $value){
-                    if($index == 0){
-                        $travel = $value;
-                        continue;
-                    }
-                    $travel->expense_amount += $value->expense_amount;
-                }
-                $travel->subledger = 'Travel';
-            return $travel;
-        });
-        $summaries = $expenses->merge($itineraries)->groupBy('activity_code_id')->flatten(1);
-
-
-        return view('TravelRequest::TravelClaim.print')
-            ->withSummaries($summaries)
-            ->withTravelClaim($travelClaim);
+        return view('TravelRequest::TravelClaim.print', compact('travelClaim'));
     }
 
 }

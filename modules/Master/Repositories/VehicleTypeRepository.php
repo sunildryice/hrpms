650e39896541f141b04720356f3720a93c2106d3
UD-Shrestha <uday@dryicesolutions.net>
2026-03-03 16:48:18 +0545

    VehicleRequest: selective vehicle types dopdown in vehicle request

--- modules/Master/Repositories/VehicleTypeRepository.php
+++ modules/Master/Repositories/VehicleTypeRepository.php
@@ -10,4 +10,16 @@ class VehicleTypeRepository extends Repository
     {
         $this->model = $vehicleType;
     }
+
+    public function getSelectiveVehicleTypes()
+    {
+        return $this->model
+            ->whereIn('title', [
+                'Car',
+                'Pick up Jeep',
+            ])
+            // ->orWhereIn('id', [3, 6]) 
+            ->orderBy('title')
+            ->get();
+    }
 }

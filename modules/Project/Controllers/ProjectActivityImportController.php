eb340eaca673ab6d1cf58d20ca2eedc4c08bb284
UD-Shrestha <uday@dryicesolutions.net>
2026-04-08 16:22:32 +0545

    update activity import

--- modules/Project/Controllers/ProjectActivityImportController.php
+++ modules/Project/Controllers/ProjectActivityImportController.php
@@ -22,35 +22,6 @@ class ProjectActivityImportController extends Controller
         return view('Project::ProjectActivity.import', compact('project'));
     }
 
-    // public function store(Request $request, Project $project)
-    // {
-    //     $request->validate([
-    //         'attachment' => 'required|max:10240|mimes:xlsx'
-    //     ], [
-    //         'attachment.required' => 'Please choose the file.',
-    //         'attachment.max' => 'File size cannot exceed :max KB',
-    //         'attachment.mimes' => 'Please upload excel file!'
-    //     ]);
-
-    //     $file = $request->hasFile('attachment') ? $request->file('attachment') : null;
-    //     try {
-
-    //         DB::beginTransaction();
-
-    //         Excel::import(new ActivityImport($project), $file, null, \Maatwebsite\Excel\Excel::XLSX);
-    //         $response = ['message' => ' Activity imported successfully.'];
-
-    //         DB::commit();
-
-    //         return response()->json($response, 200);
-    //     } catch (\Throwable $th) {
-
-    //         DB::rollBack();
-    //         dd($th);
-    //         $response = ['message' => 'Failed to import activity. Please check the file and try again.'];
-    //         return response()->json($response, 500);
-    //     }
-    // }
 
     public function store(Request $request, Project $project)
     {

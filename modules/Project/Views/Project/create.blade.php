a1912cf4692a912c2185690c497ee8913bfb5ec9
UD-Shrestha <uday@dryicesolutions.net>
2026-04-16 15:06:56 +0545

    update for additional fields of projects

--- modules/Project/Views/Project/create.blade.php
+++ modules/Project/Views/Project/create.blade.php
@@ -114,16 +114,16 @@
                 }
             });
 
-            $('.select2').on('change', function() {
-                const fieldName = $(this).attr('name');
-                if (fieldName) {
-                    fv.revalidateField(fieldName);
-                }
-            });
+            // $('.select2').on('change', function() {
+            //     const fieldName = $(this).attr('name');
+            //     if (fieldName) {
+            //         fv.revalidateField(fieldName);
+            //     }
+            // });
 
-            $('.select2[multiple]').on('select2:select select2:unselect', function() {
-                fv.revalidateField($(this).attr('name'));
-            });
+            // $('.select2[multiple]').on('select2:select select2:unselect', function() {
+            //     fv.revalidateField($(this).attr('name'));
+            // });
 
             $('[data-toggle="datepicker"]').on('change', function() {
                 fv.revalidateField($(this).attr('name'));
--- modules/Project/Views/Project/edit.blade.php
+++ modules/Project/Views/Project/edit.blade.php
@@ -121,16 +121,16 @@
             });
 
             // Keep validation flow consistent with create view
-            $('.select2').on('change', function() {
-                const fieldName = $(this).attr('name');
-                if (fieldName) {
-                    fv.revalidateField(fieldName);
-                }
-            });
+            // $('.select2').on('change', function() {
+            //     const fieldName = $(this).attr('name');
+            //     if (fieldName) {
+            //         fv.revalidateField(fieldName);
+            //     }
+            // });
 
-            $('.select2[multiple]').on('select2:select select2:unselect', function() {
-                fv.revalidateField($(this).attr('name'));
-            });
+            // $('.select2[multiple]').on('select2:select select2:unselect', function() {
+            //     fv.revalidateField($(this).attr('name'));
+            // });
 
             $('[data-toggle="datepicker"]').on('change', function() {
                 fv.revalidateField($(this).attr('name'));

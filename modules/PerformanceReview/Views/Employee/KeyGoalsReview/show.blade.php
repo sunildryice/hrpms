bcb2bd69e34f5ce7735fb1ff38eb3575fcad22b4
UD-Shrestha <uday@dryicesolutions.net>
2026-06-03 11:16:17 +0545

    update show page in employee key goal review

--- modules/PerformanceReview/Views/Employee/KeyGoalsReview/show.blade.php
+++ modules/PerformanceReview/Views/Employee/KeyGoalsReview/show.blade.php
@@ -283,7 +283,8 @@
                                     <tr class="devplan-row readonly">
                                         <td class="sn">{{ $loop->iteration }}</td>
                                         <td class="col-plan readonly-cell wrap-text">
-                                            {{ $plan->objective }}
+                                            {{-- {{ $plan->objective }} --}}
+                                            {!! $plan->objective ? nl2br(e($plan->objective)) : '—' !!}
                                         </td>
                                     </tr>
                                 @endforeach

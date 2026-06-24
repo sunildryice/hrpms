de2ac3bfb5c0680480522da5536a51a8a1ca17fe
UD-Shrestha <uday@dryicesolutions.net>
2026-06-24 11:12:06 +0545

    Update for Project Activity Detail

--- database/migrations/2026_06_23_162245_create_project_activity_details_table.php
+++ database/migrations/2026_06_23_162245_create_project_activity_details_table.php
@@ -4,22 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up()
     {
         Schema::create('project_activity_details', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('project_activity_id');
-            $table->text('key_accomplishment');
-            $table->text('challenge');
-            $table->text('lesson_learned');
+            $table->text('key_accomplishment')->nullable();
+            $table->text('challenge')->nullable();
+            $table->text('lesson_learned')->nullable();
             $table->unsignedBigInteger('created_by')->nullable();
             $table->unsignedBigInteger('updated_by')->nullable();
+            $table->nullableTimestamps();
+
             $table->foreign('project_activity_id')->references('id')->on('project_activities')->onDelete('cascade');
             $table->foreign('created_by')->references('id')->on('users')->onDelete('set null');
             $table->foreign('updated_by')->references('id')->on('users')->onDelete('set null');
-            $table->timestamps();
         });
     }
 
